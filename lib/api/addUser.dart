83553a32f29d7a08c4d56a7cf4a9e5721f02fe5c
Rajveer070 <22010101179@darshan.ac.in>
2024-03-17 18:18:31 +0530

    first commit

---
+++ lib/api/addUser.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class AddUser extends StatefulWidget {
+  final Map<String, dynamic>? map;
+
+  const AddUser(this.map, {Key? key}) : super(key: key);
+
+  @override
+  State<AddUser> createState() => _AddUserState();
+}
+
+class _AddUserState extends State<AddUser> {
+  final TextEditingController nameController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.map != null && widget.map!["name"] != null) {
+      nameController.text = widget.map!["name"].toString();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.map == null ? "Add User" : "Update User"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextFormField(
+              controller: nameController,
+              decoration: const InputDecoration(
+                labelText: "Name",
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                if (widget.map == null) {
+                  await addUser(nameController.text);
+                } else {
+                  await updateUser(
+                      widget.map!["id"].toString(), nameController.text);
+                }
+                Navigator.of(context).pop();
+              },
+              child: Text(widget.map == null ? "Add User" : "Update User"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<void> addUser(String name) async {
+    var res = await http.post(
+      Uri.parse("https://65f14ed8da8c6584131d591c.mockapi.io/Users"),
+      body: {"name": name},
+    );
+  }
+
+  Future<void> updateUser(String id, String newName) async {
+    var res = await http.put(
+      Uri.parse("https://65f14ed8da8c6584131d591c.mockapi.io/Users/$id"),
+      body: {"name": newName},
+    );
+  }
+}
